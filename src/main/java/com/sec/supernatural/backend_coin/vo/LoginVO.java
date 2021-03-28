de644b3be47ac497230c0c7695437cd4cfd1cdce
王雨晨 <181250147@smail.nju.edu.cn>
2021-03-28 13:06:51 +0800

    feat: 登录

---
+++ src/main/java/com/sec/supernatural/backend_coin/vo/LoginVO.java
@@ -0,0 +1,13 @@
+package com.sec.supernatural.backend_coin.vo;
+
+import lombok.Data;
+
+/**
+ * @author wangyuchen
+ * @date 2021/3/27 11:08 上午
+ */
+@Data
+public class LoginVO {
+    private String username;
+    private String password;
+}
