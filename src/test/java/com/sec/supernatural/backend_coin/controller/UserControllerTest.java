41bf2893e24546bf2b0ae0dcfc759c8ae0832fa4
S1XCamus <2624020095@qq.com>
2021-04-09 23:33:20 +0800

    feat: test

---
+++ src/test/java/com/sec/supernatural/backend_coin/controller/UserControllerTest.java
@@ -0,0 +1,16 @@
+package com.sec.supernatural.backend_coin.controller;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author shenyichen
+ * @date 2021/4/9
+ **/
+@RunWith(SpringRunner.class)
+@SpringBootTest
+//@WebAppConfiguration
+public class UserControllerTest {
+    // 采用postman测试
+}
