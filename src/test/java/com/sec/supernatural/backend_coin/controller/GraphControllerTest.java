824bd0b659b4c8cd042594f223506650bb7d655c
王雨晨 <181250147@smail.nju.edu.cn>
2021-04-11 16:17:03 +0800

    feat: controller测试生成token/UserController部分测试

--- src/test/java/com/sec/supernatural/backend_coin/controller/GraphControllerTest.java
+++ src/test/java/com/sec/supernatural/backend_coin/controller/GraphControllerTest.java
@@ -1,16 +1,82 @@
 package com.sec.supernatural.backend_coin.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.sec.supernatural.backend_coin.constant.MyResponse;
+import com.sec.supernatural.backend_coin.util.JwtUtil;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
 /**
  * @author shenyichen
  * @date 2021/4/9
  **/
-@RunWith(SpringRunner.class)
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
 //@WebAppConfiguration
 public class GraphControllerTest {
-    // 采用postman测试
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setupMockMvc(){
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    private MyResponse postTemplate(String url, Object object, int expect_code, Map<String, Object> headers) throws Exception{
+
+        MockHttpServletRequestBuilder mockHttpServletRequestBuilder = MockMvcRequestBuilders
+                .post(url)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Accept-Encoding", "gzip, deflate")
+                .header("Connection", "keep-alive")
+                .header("Accept-Language", "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7")
+                .header("token", JwtUtil.createToken(1, 24))
+                .contentType("application/json;charset=UTF-8");
+
+        if(object != null){
+            String jsonResult = JSONObject.toJSONString(object);
+            mockHttpServletRequestBuilder = mockHttpServletRequestBuilder.content(jsonResult);
+        }
+
+        if(headers != null){
+            for (Map.Entry<String, Object> entry : headers.entrySet()) {
+                mockHttpServletRequestBuilder = mockHttpServletRequestBuilder.header(entry.getKey(), entry.getValue());
+            }
+        }
+
+        MvcResult mvcResult = mockMvc.perform(mockHttpServletRequestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        String result = mvcResult.getResponse().getContentAsString();
+
+        MyResponse myResponse = JSONObject.parseObject(result, MyResponse.class);
+
+        if(myResponse.getCode() != expect_code){
+            System.out.println(myResponse.getCode());
+            System.out.println(new String(myResponse.getData().toString()
+                    .getBytes(StandardCharsets.ISO_8859_1), "UTF-8"));
+        }
+
+        assert myResponse.getCode() == expect_code;
+        return myResponse;
+    }
 }
