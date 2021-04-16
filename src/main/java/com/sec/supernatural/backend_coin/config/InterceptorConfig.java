e8446d99a656002e63b3b789376d9723f89144df
王雨晨 <181250147@smail.nju.edu.cn>
2021-04-16 11:03:00 +0800

    fix: 开放文件访问

--- src/main/java/com/sec/supernatural/backend_coin/config/InterceptorConfig.java
+++ src/main/java/com/sec/supernatural/backend_coin/config/InterceptorConfig.java
@@ -15,6 +15,7 @@ public class InterceptorConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new JWTInterceptor())
                 .addPathPatterns("/**")
-                .excludePathPatterns("/api/user/login");
+                .excludePathPatterns("/api/user/login")
+                .excludePathPatterns("/api/storage/**");
     }
 }
