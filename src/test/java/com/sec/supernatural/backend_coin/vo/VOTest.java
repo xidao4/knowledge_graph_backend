00d9a275c2cd3205696952310ab702db23216dd4
S1XCamus <2624020095@qq.com>
2021-05-07 19:18:12 +0800

    test: 覆盖率

--- src/test/java/com/sec/supernatural/backend_coin/vo/VOTest.java
+++ src/test/java/com/sec/supernatural/backend_coin/vo/VOTest.java
@@ -1,5 +1,6 @@
 package com.sec.supernatural.backend_coin.vo;
 
+import com.sec.supernatural.backend_coin.po.PicUnit;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,5 +19,65 @@ public class VOTest {
         BindUrlToPicVO bindUrlToPicVO = new BindUrlToPicVO();
         bindUrlToPicVO.hashCode();
         bindUrlToPicVO.toString();
+        // EdgeVO
+        EdgeVO edgeVO = new EdgeVO();
+        edgeVO.hashCode();
+        edgeVO.toString();
+        // GraphVO
+        GraphVO graphVO = new GraphVO();
+        graphVO.hashCode();
+        graphVO.toString();
+        // LoginVO
+        LoginVO loginVO = new LoginVO();
+        loginVO.hashCode();
+        loginVO.toString();
+        // NodesByTypesVO
+        NodesByTypesVO nodesByTypesVO = new NodesByTypesVO();
+        nodesByTypesVO.hashCode();
+        nodesByTypesVO.toString();
+        // NodeVO
+        NodeVO nodeVO = new NodeVO();
+        nodeVO.hashCode();
+        nodeVO.toString();
+        // PicElementVO
+        PicElementVO picElementVO = new PicElementVO();
+        picElementVO.hashCode();
+        picElementVO.toString();
+        // PicIdVO
+        PicIdVO picIdVO = new PicIdVO();
+        picIdVO.hashCode();
+        picIdVO.toString();
+        // PicTypesVO
+        PicTypesVO picTypesVO = new PicTypesVO();
+        picTypesVO.hashCode();
+        picTypesVO.toString();
+        // PicUnitVO
+        PicUnitVO picUnitVO = new PicUnitVO();
+        picUnitVO.hashCode();
+        picUnitVO.toString();
+        // SearchResultVO
+        SearchResultVO searchResultVO = new SearchResultVO();
+        searchResultVO.hashCode();
+        searchResultVO.toString();
+        // SearchVO
+        SearchVO searchVO = new SearchVO();
+        searchVO.hashCode();
+        searchVO.toString();
+        // ThumbnailVO
+        ThumbnailVO thumbnailVO = new ThumbnailVO();
+        thumbnailVO.hashCode();
+        thumbnailVO.toString();
+        // UserIdVO
+        UserIdVO userIdVO = new UserIdVO();
+        userIdVO.hashCode();
+        userIdVO.toString();
+        // UserPicVO
+        UserPicVO userPicVO = new UserPicVO();
+        userPicVO.hashCode();
+        userPicVO.toString();
+        // UserVO
+        UserVO userVO = new UserVO();
+        userVO.hashCode();
+        userVO.toString();
     }
 }
