6b1db971380f7dc8b3316a783920ca4dd5b56fdb
S1XCamus <2624020095@qq.com>
2021-06-18 14:48:36 +0800

    feat: add test controller chat and search

--- src/test/java/com/sec/supernatural/backend_coin/controller/ChatControllerTest.java
+++ src/test/java/com/sec/supernatural/backend_coin/controller/ChatControllerTest.java
@@ -1,12 +1,13 @@
-package com.sec.supernatural.backend_coin.bl;
+package com.sec.supernatural.backend_coin.controller;
 
+import com.sec.supernatural.backend_coin.vo.application.request.ChatVO;
 import org.apache.http.entity.ContentType;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -18,15 +19,18 @@ import java.io.IOException;
  * @author shenyichen
  * @date 2021/6/18
  **/
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class ChatServiceTest {
+@ExtendWith(SpringExtension.class)
+public class ChatControllerTest {
     @Autowired
-    ChatService chatService;
+    ChatController chatController;
 
     @Test
-    public void getAnswer() throws Exception {
-        chatService.getAnswer("0","林黛玉的结局");
+    public void getAnswer(){
+        ChatVO chatVO = new ChatVO();
+        chatVO.setRoleId("0");
+        chatVO.setQuestion("林黛玉的结局");
+        chatController.getAnswer(chatVO);
     }
 
     @Test
@@ -35,6 +39,6 @@ public class ChatServiceTest {
         FileInputStream inputStream = new FileInputStream(file);
         MultipartFile mfile = new MockMultipartFile(file.getName(), file.getName(),
                 ContentType.APPLICATION_OCTET_STREAM.toString(), inputStream);
-        chatService.uploadScene(mfile);
+        chatController.uploadScene(mfile);
     }
 }
---
+++ src/test/java/com/sec/supernatural/backend_coin/controller/SearchControllerTest.java
@@ -0,0 +1,50 @@
+package com.sec.supernatural.backend_coin.controller;
+
+import com.sec.supernatural.backend_coin.vo.application.request.NodeLabel;
+import com.sec.supernatural.backend_coin.vo.application.request.SemanticSearchVO;
+import org.apache.http.entity.ContentType;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * @author shenyichen
+ * @date 2021/6/18
+ **/
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+public class SearchControllerTest {
+    @Autowired
+    SearchController searchController;
+
+    @Test
+    public void getAnswer(){
+        SemanticSearchVO semanticSearchVO = new SemanticSearchVO();
+        semanticSearchVO.setQuestion("林黛玉最后怎么了");
+        searchController.getAnswer(semanticSearchVO);
+    }
+
+    @Test
+    public void uploadScene() throws IOException {
+        File file = new File("datastore\\1.jpg");
+        FileInputStream inputStream = new FileInputStream(file);
+        MultipartFile mfile = new MockMultipartFile(file.getName(), file.getName(),
+                ContentType.APPLICATION_OCTET_STREAM.toString(), inputStream);
+        searchController.uploadScene(mfile);
+    }
+
+    @Test
+    public void detail(){
+        NodeLabel nodeLabel = new NodeLabel();
+        nodeLabel.setLabel("林黛玉");
+        searchController.detail(nodeLabel);
+    }
+}
