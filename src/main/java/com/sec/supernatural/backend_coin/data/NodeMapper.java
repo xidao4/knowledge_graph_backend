5c8e55dd88f4712cb1ac2797adff552b934fca45
刘佳月 <181250086@smail.nju.edu.cn>
2021-06-18 20:48:51 +0800

    fix: 删除mapper层不用的方法

--- src/main/java/com/sec/supernatural/backend_coin/data/NodeMapper.java
+++ src/main/java/com/sec/supernatural/backend_coin/data/NodeMapper.java
@@ -9,12 +9,12 @@ import java.util.List;
 @Repository
 public interface NodeMapper {
 
-    public void insert(Node node);
+//    public void insert(Node node);
 
-    public void deleteNodeByName(String label,String picId);
-    public void updateNode(Node newNode,String oldLabel,String picId);
+//    public void deleteNodeByName(String label,String picId);
+//    public void updateNode(Node newNode,String oldLabel,String picId);
     public Node findByName(String label,String picId);
-    public Node retrieveNodesByPicId(String picId);
+//    public Node retrieveNodesByPicId(String picId);
 
     public List<Node> getNeighborsByLabel(String label,String picId);
 
